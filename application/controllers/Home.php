146876679ad203662ae0dd08fc730fd6c0ec7467
PankajBrightcodess <86092953+PankajBrightcodess@users.noreply.github.com>
2023-11-27 11:14:29 +0530

    ddd

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -70,7 +70,7 @@ class Home extends CI_Controller {
 		// $data['user']=$this->Account_model->getuser(array("md5(id)"=>$this->session->userdata('user')));
 		// $regid=$data['user']['id'];
 		// $pin=$this->Epin_model->getepinbystatus('0',$regid);
-		
+		//https://admin.codezealtech.in/dream_touch/pull.php
 		$this->template->load('pages','home',$data);
 	}
 	
